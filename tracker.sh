044b70122b4f961faf67ca30db53cc9658c32943
Petterson Faria <contato@pettersonfaria.com.br>
2012-06-13 15:53:24 -0300

    adicionando novos trackers e fazendo ajuste no script

--- tracker.sh
+++ tracker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 temp="/tmp/tracker.txt"
 origin="tracker.txt"
-> $temp
+if [ -f $temp ]; then
+  > $temp
+fi
+sed -i '/^$/D' $origin
 for i in `sort -u $origin`; do
   echo $i >> $temp
   echo >> $temp
 done
 
-mv $temp $origin && gedit $origin
+mv $temp $origin && gedit $origin &
